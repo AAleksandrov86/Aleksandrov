2aa15a17c2a5ea5835a12d7f7bc6c51ad6847df3
AAleksandrov86 <ac_86@mail.ru>
2017-10-17 21:35:37 +0700

    Сделаны задания 1,2,3,4

---
+++ Лабораторная1/Задание4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a,b,c;
+            a = Console.ReadLine();
+            Console.Write("Программирование это ");
+            Console.Write(a);
+            Console.Write("!");
+            
+
+
+
+
+
+
+
+
+        }
+    }
+}
