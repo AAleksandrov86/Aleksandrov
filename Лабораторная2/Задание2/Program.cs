4d5e3c6454e00d4358e2269ebcf062fd8b52b698
AAleksandrov86 <ac_86@mail.ru>
2017-10-18 21:28:33 +0700

    Сделано 3 задание 2Лабораторной

---
+++ Лабораторная2/Задание2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a;
+            a = Console.ReadLine();
+            double A;
+            A = double.Parse(a);
+            double S;
+            S =  6*(A*A);
+            Console.WriteLine(S);
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Лабораторная2/Задача3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String a;
+            a = Console.ReadLine();
+            double A;
+            A = double.Parse(a);
+            double S;
+            S = A*A*A;
+            Console.WriteLine(S);
+
+
+
+
+        }
+    }
+}
