b74451118f38adc7a5876afbae0d79fdd08c6563
Aleksandrov <ac_86@mail.ru>
2017-10-18 14:43:51 +0600

    Сделано 5 задание

--- Лабораторная1/Задание6а/Program.cs
+++ Лабораторная1/Задание6а/Program.cs
@@ -4,18 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Задание6
+namespace Задание6а
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string a,b;
-            a = Console.ReadLine();
-            b = Console.ReadLine();
-            Console.Write(b);
-            Console.Write(" ");
-            Console.Write(a);
 
 
 
