5375d1818d52827470a914df69eb961d69ccc000
Aleksandrov <ac_86@mail.ru>
2017-10-24 14:58:24 +0600

    Лаб1 сделана полностью

--- Лабораторная1/Задача12/Program.cs
+++ Лабораторная1/Задача12/Program.cs
@@ -4,27 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Задача11
+namespace Задача12
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            string a,b,c,d;
+            string a, b, c, d, e, f;
             a = Console.ReadLine();
             b = Console.ReadLine();
             c = Console.ReadLine();
             d = Console.ReadLine();
-            int A,B,C;
-            A = int.Parse(a);
-            B = int.Parse(b);
-            C = int.Parse(c);
+            e = Console.ReadLine();
             
-
-
-            Console.WriteLine("<circle cx=\""+a+"\" cy=\""+b+"\"\n" +
-                "r=\""+c+"\" fill=\""+d+"\" /> ");
+            Console.WriteLine("User ID="+a+";" +
+                "Password="+b+";\n" +
+                "Host="+c+";" +
+                "Port="+d+";" +
+                "Database="+e+";");
                 
 
 
